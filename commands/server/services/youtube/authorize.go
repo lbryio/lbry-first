1cd40232b6dd0016b7cb906d7c3945ebb398eb00
Mark Beamer Jr <markbeamerjr@gmail.com>
2020-03-08 23:31:40 -0400

    fix testing error

--- commands/server/services/youtube/authorize.go
+++ commands/server/services/youtube/authorize.go
@@ -113,7 +113,7 @@ func getTokenFromWeb(config *oauth2.Config, authURL string) (*oauth2.Token, erro
 		log.Fatalf("Unable to open authorization URL in web server: %v", err)
 	} else {
 		fmt.Println("Your browser has been opened to an authorization URL.",
-			" This program will resume once authorization has been provided.\n")
+			" This program will resume once authorization has been provided.")
 		fmt.Println(authURL)
 	}
 
